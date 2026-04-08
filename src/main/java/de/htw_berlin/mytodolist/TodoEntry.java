d3301b681a821794e1557ab681a61aed27db3c37
Prof. Dr. Arif Wider <wider@htw-berlin.de>
2026-04-08 10:27:28 +0200

    created simple entity class

---
+++ src/main/java/de/htw_berlin/mytodolist/TodoEntry.java
@@ -0,0 +1,28 @@
+package de.htw_berlin.mytodolist;
+
+public class TodoEntry {
+    String title;
+    boolean done;
+
+    public TodoEntry() {}
+
+    public TodoEntry(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
